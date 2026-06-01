8befc983c2ff1c227b96e22d500b6963436fc7e5
adinmaccabee <134990891+adinmaccabee@users.noreply.github.com>
2026-06-01 21:27:13 +0100

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -13,7 +13,7 @@ done
 
 # register wallpapers in background settings
 mkdir -p ~/.local/share/gnome-background-properties
-cat > ~/.local/share/gnome-background-properties/yaru-wallpapers.xml << XML
+cat > ~/.local/share/gnome-background-properties/Yaru.xml << XML
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
 <wallpapers>
